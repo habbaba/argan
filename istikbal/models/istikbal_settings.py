f1092b049c7def356f72a92175c3349e5e9b97a9
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2022-07-22 14:24:46 +0500

    Add files via upload

---
+++ istikbal/models/istikbal_settings.py
@@ -0,0 +1,17 @@
+from odoo import _, api, fields, models, modules, SUPERUSER_ID, tools
+from odoo.exceptions import ValidationError, UserError
+import json
+import requests
+import base64
+import time
+from datetime import datetime, timedelta
+
+
+class Credentials(models.Model):
+    _name = 'istikbal.credentials'
+
+    username = fields.Char('Username')
+    password = fields.Char('Password')
+    company_id = fields.Many2one('res.company', string='Company', required=True, readonly=True,
+                                 default=lambda self: self.env.company)
+    active = fields.Boolean('Active')
