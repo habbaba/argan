60925266dd2755dc39106e3621f39354e2ebede8
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2022-08-24 12:58:30 +0500

    Add files via upload

--- arga_transport/__manifest__.py
+++ arga_transport/__manifest__.py
@@ -16,13 +16,14 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': 'v15 enterprise',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','account'],
 
     # always loaded
     'data': [
+         'data/seq.xml',
          'security/ir.model.access.csv',
          'views/arga_transport.xml',
         
--- arga_transport/models/arga_transport.py
+++ arga_transport/models/arga_transport.py
@@ -5,22 +5,22 @@ from odoo import models, fields, api
 class ArgaTranport(models.Model):
     _name="arga.transport"
     
-    name= fields.Char(string = 'Tour Nr')
+    name= fields.Char(string = 'Tour Nr', readonly=True, index=True, copy=False, default='New')
     date_load= fields.Date(string = 'Date of Load')
     from_country= fields.Many2one('res.country' ,string = 'From')
     to_country= fields.Many2one('res.country' ,string = 'To')
     customer_id= fields.Many2one('res.partner' ,string = 'Customer')
     inc_nr= fields.Char(string = 'Inc nr')
-    cost_without_vat= fields.Float(string = 'Cost without vat')
-    vat19 = fields.Float(string = 'Vat 19%')
+    cost_without_vat= fields.Float(string = 'Cost without vat', readonly=True,copy=False)
+    vat19 = fields.Float(string = 'Vat 19%' , readonly=True, copy=False)
     cost_total = fields.Float(string = 'Cost Total', compute ='clacTotalCost' ,store=True)
-    cost_paid = fields.Float(string = 'Paid',)
+    cost_paid = fields.Float(string = 'Paid', readonly=True, copy=False)
     open_cost= fields.Float(string = 'Open', compute='calcOpenCost' ,store=True)
     vendor_id= fields.Many2one('res.partner' ,string = 'Vendor( CARRIER )')
     incoming_inc= fields.Char(string = 'Incoming inc')
-    vend_cost= fields.Float(string = 'Cost')
-    vat_zero = fields.Float(string = 'Vat 0%')
-    vend_paid= fields.Float(string = 'Paid 2')
+    vend_cost= fields.Float(string = 'Cost' ,copy=False ,readonly= True)
+    vat_zero = fields.Float(string = 'Vat 0%' ,copy=False ,readonly= True)
+    vend_paid= fields.Float(string = 'Paid 2' ,copy=False ,readonly= True)
     vend_open= fields.Float(string = 'Open 2' ,compute='calcOpenSecCost',store=True)
     truck_nr =fields.Char(string = 'Truck nr')
     plate_nr =fields.Char(string = 'Plate nr')
@@ -33,9 +33,46 @@ class ArgaTranport(models.Model):
     spalte_four =fields.Char(string = 'Spalte 4')
     spalte_five = fields.Char(string = 'Spalte 5')
     spalte_six =fields.Char(string = 'Spalte 6')
+    customer_inv = fields.Many2one('account.move' , string="Invoice")
+    vendor_bill = fields.Many2one('account.move' , string="Bill")
+
+    @api.model
+    def create(self, vals):
+       
+        if vals.get('name', 'New') == 'New':
+          
+            vals['name'] = self.env['ir.sequence'].next_by_code('arga.transport') or '/'
+      
+        res = super(ArgaTranport, self).create(vals)
+        
+        return res
+   
+    @api.onchange('customer_inv')
+    def getInvoiceData(self):
+        if self.customer_inv:
+            self.cost_without_vat = self.customer_inv.amount_untaxed
+            self.vat19 = self.customer_inv.amount_tax
+            if self.customer_inv._get_reconciled_payments():
+                total_paid =  sum(self.customer_inv._get_reconciled_payments().mapped('amount')) 
+                self.cost_paid  =total_paid
+            
     
     
     
+    
+    @api.onchange('vendor_bill')
+    def getInvoiceBillData(self):
+        if self.vendor_bill:
+            self.vend_cost = self.vendor_bill.amount_untaxed
+            self.vat_zero = self.vendor_bill.amount_tax
+            if self.vendor_bill._get_reconciled_payments():
+                total_paid =  sum(self.vendor_bill._get_reconciled_payments().mapped('amount')) 
+                self.vend_paid  =total_paid
+    
+    
+    
+    
+            
     @api.depends('cost_without_vat','vat19')
     def clacTotalCost(self):
         for rec in self:
