9818bf964ad89b7b4b03e024fa4b1267744831f2
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2022-12-10 16:11:38 +0500

    Update bellona_schedulers.py

--- exim_bellona/models/bellona_schedulers.py
+++ exim_bellona/models/bellona_schedulers.py
@@ -42,7 +42,7 @@ class Integration(models.TransientModel):
                 self.env.cr.commit()
             else:
                 log_notes = self.env["bellona.log.notes"].sudo().create(
-                    {"error": "shipment"+company.company_id.name + ": " + str(response)})
+                    {"error": "shipment"+company.company_id.name + ": " + str(response.text)})
 
     def createShipmentsScheduler(self, shipments,company_id):
         for shipment in shipments:
