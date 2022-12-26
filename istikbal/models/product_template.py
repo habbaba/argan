cb410aaa9ac7952d3e7637eed11b7981781f0f96
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2022-12-26 15:52:33 +0500

    Add files via upload

--- istikbal/models/product_template.py
+++ istikbal/models/product_template.py
@@ -192,45 +192,48 @@ class IstikbalSaleOrderInh(models.Model):
 
     istikbal_shipments = fields.Many2many('istikbal.incoming.shipments', string='Istikbal Inventory',compute="compute_the_shipments")
     istikbal_shp_details = fields.Many2many('istikbal.shipments.details', string='Istikbal Shipment details',compute="compute_the_shipments")
+    delivery_tags = fields.Many2many('crm.tag', 'name',string='Delivery Tags',compute="compute_the_shipments")
 
     def compute_the_shipments(self):
         for i in self:
             shipments = self.env['purchase.order'].search([('origin', '=', i.name)])
             i.istikbal_shipments=shipments.istikbal_shipments
             i.istikbal_shp_details=shipments.istikbal_shp_details
-#             if shipments:
-#                 tags = self.env['crm.tag'].search([('name', '=', "bestellt")])
-#                 if not tags:
-#                     tags = self.env['crm.tag'].create({'name': "bestellt"})
+            tags = self.env['crm.tag'].search([('name', '=', "bestellt")])
+            i.delivery_tags
+            if shipments:
+                tags = self.env['crm.tag'].search([('name', '=', "bestellt")])
+                if not tags:
+                    tags = self.env['crm.tag'].create({'name': "bestellt"})
 
-#                 i.tag_ids = tags.ids
+                i.delivery_tags = tags.ids
 
-#             if i.istikbal_shipments:
-#                 if not i.istikbal_shp_details:
-#                     tags= self.env['crm.tag'].search([('name', '=', "Production")])
-#                     if not tags:
-#                         tags = self.env['crm.tag'].create({'name': "Production"})
+            if i.istikbal_shipments:
+                if not i.istikbal_shp_details:
+                    tags= self.env['crm.tag'].search([('name', '=', "Production")])
+                    if not tags:
+                        tags = self.env['crm.tag'].create({'name': "Production"})
 
-#                     i.tag_ids= tags.ids
+                    i.delivery_tags= tags.ids
 
-#             if i.istikbal_shipments and i.istikbal_shp_details:
-#                     tags = self.env['crm.tag'].search([('name', '=', "On Truck")])
-#                     if not tags:
-#                         tags = self.env['crm.tag'].create({'name': "On Truck"})
+            if i.istikbal_shipments and i.istikbal_shp_details:
+                    tags = self.env['crm.tag'].search([('name', '=', "On Truck")])
+                    if not tags:
+                        tags = self.env['crm.tag'].create({'name': "On Truck"})
 
-#                     i.tag_ids = tags.ids
+                    i.delivery_tags = tags.ids
 
-#             for k in i.picking_ids:
-#                 if k.state=="done":
-#                     tags = self.env['crm.tag'].search([('name', '=', "GG")])
-#                     if not tags:
-#                         tags = self.env['crm.tag'].create({'name': "GG"})
-#                     i.tag_ids = tags.ids
-#                 if k.state=="assigned":
-#                     tags = self.env['crm.tag'].search([('name', '=', "LB")])
-#                     if not tags:
-#                         tags = self.env['crm.tag'].create({'name': "LB"})
-#                     i.tag_ids = tags.ids
+            for k in i.picking_ids:
+                if k.state=="done":
+                    tags = self.env['crm.tag'].search([('name', '=', "GG")])
+                    if not tags:
+                        tags = self.env['crm.tag'].create({'name': "GG"})
+                    i.delivery_tags = tags.ids
+                if k.state=="assigned":
+                    tags = self.env['crm.tag'].search([('name', '=', "LB")])
+                    if not tags:
+                        tags = self.env['crm.tag'].create({'name': "LB"})
+                    i.delivery_tags = tags.ids
 
 
 
