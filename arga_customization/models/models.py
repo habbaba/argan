8c0f0ad828cf8a9d1b927b188f7831ee71f104b5
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-03-02 15:07:27 +0500

    Update models.py

--- arga_customization/models/models.py
+++ arga_customization/models/models.py
@@ -240,7 +240,31 @@ class PurchaseOrderInh(models.Model):
         for rec in self:
             sale_order=self.env['sale.order'].search([("name",'=',rec.origin)],limit=1).id
             rec.sale_order=sale_order
-           
+            
+            
+class PurchaseOrderInh(models.Model):
+    _inherit = 'purchase.order'
+
+    total_lines = fields.Integer(compute='_compute_lines')
+    
+
+    @api.depends('order_line')
+    def _compute_lines(self):
+        self.total_lines = len(self.order_line.mapped('id'))
+
+
+class PurchaseOrderLineInh(models.Model):
+    _inherit = 'purchase.order.line'
+
+    number = fields.Integer(compute='_compute_get_number', store=True)
+
+    @api.depends('sequence', 'order_id')
+    def _compute_get_number(self):
+        for order in self.mapped('order_id'):
+            number = 1
+            for line in order.order_line:
+                line.number = number
+                number += 1           
 
 
 
