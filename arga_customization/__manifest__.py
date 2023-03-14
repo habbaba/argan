a197da635c5bb939788e5dd66284649b5244a101
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-03-14 17:33:54 +0500

    Update __manifest__.py

--- arga_customization/__manifest__.py
+++ arga_customization/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'stock', 'calendar', 'account', 'purchase'],
+    'depends': ['base', 'sale', 'stock', 'calendar', 'account', 'purchase','istikbal'],
 
     # always loaded
     'data': [
