caab4fa64c0ed5f2d609a078335709eea83c4a98
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-03-15 17:50:38 +0500

    Update combine_shipments.py

--- istikbal/models/combine_shipments.py
+++ istikbal/models/combine_shipments.py
@@ -58,7 +58,7 @@ class IstikbalLogNotes(models.Model):
                             #     r.is_received = True
                             # if r.productCode in products_codes:
                             if not r.picking_id:
-                               stml = self.env['stock.move.line'].search([('origin','=', r.purchase_id.name)],limit=1)
+                                stml = self.env['stock.move.line'].search([('origin','=', r.purchase_id.name)],limit=1)
                                 r.picking_id = stml.picking_id.id
                                 if r.picking_id.state == 'done':
                                     r.is_received = True
